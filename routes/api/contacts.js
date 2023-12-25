70ad02b98c40e45ffd487c33267dacffeee95985
wosean <serhii_v@ukr.net>
2023-12-26 00:47:43 +0200

    task done hw03

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,28 @@
 const express = require('express')
 
+const ctrl = require("../../controllers/contacts");
+
+const { validateBody, isValidId, isEmptyBody } = require("../../middlewares");
+
+const { schemas } = require("../../models/contact");
+
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", ctrl.getAll);
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:id", isValidId, ctrl.getById);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:id", isValidId, ctrl.deleteById);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:id", isEmptyBody, isValidId, validateBody(schemas.addSchema), ctrl.updateById);
 
-module.exports = router
+router.patch(
+  "/:id/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateStatusContact
+);
+
+module.exports = router;
